bd25fce2f9fb6d9bceac1444f5652a2b633a1abd
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-02 20:11:13 +0000

    Area e volume

---
+++ Cilindro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura, raio, volume, superficie;
+            float pi = 3.1415926;
+
+            superficie = 2 * pi * raio *(raio + altura);
+            volume = pi * raio * raio * altura;
+
+            Console.WriteLine("Coloque a altura: ");
+
+            altura = Console.ReadLine();
+
+            Console.WriteLine("Coloque o raio: ");
+
+            raio = Console.ReadLine();
+
+            Console.WriteLine($"O volume é: {volume}");
+            Console.WriteLine($"A área da superficie é: {superficie}");
+
+
+        }
+    }
+}
