8521a9854c20118eb0a0b6d46cb1f08a0a0da20c
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-03 14:43:20 +0000

    Valores Maximos

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variavel1 = int.MaxValue;
+            short variavel2 = short.MaxValue;
+            byte variavel3 = byte.MaxValue;
+            double variavel4 = double.MaxValue;
+            float variavel5 = float.MaxValue;
+
+            Console.WriteLine(variavel1);
+            Console.WriteLine(variavel2);
+            Console.WriteLine(variavel3);
+            Console.WriteLine(variavel4);
+            Console.WriteLine(variavel5);
+
+
+        }
+    }
+}
