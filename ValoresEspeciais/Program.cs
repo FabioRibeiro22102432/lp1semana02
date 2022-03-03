c452d75c44e81aca0504d8ca36e34bf1dce4edb3
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-03 15:17:51 +0000

    Overflow e Underflow

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,17 +6,26 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            int variavel1 = int.MinValue;
-            short variavel2 = short.MinValue;
-            byte variavel3 = byte.MinValue;
+            int variavel1 = int.MaxValue;
+            short variavel2 = short.MaxValue;
+            byte variavel3 = byte.MaxValue;
             double variavel4 = double.NaN;
             float variavel5 = float.PositiveInfinity;
+            float variavel6;
+
+            variavel1++;
+            variavel2++;
+            variavel3++;
+            variavel6 = variavel5;
+
+
 
             Console.WriteLine(variavel1);
             Console.WriteLine(variavel2);
             Console.WriteLine(variavel3);
             Console.WriteLine(variavel4);
             Console.WriteLine(variavel5);
+            Console.WriteLine(variavel5 == variavel6 + 0.0001);
 
 
         }
