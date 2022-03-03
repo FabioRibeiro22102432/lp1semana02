157c8d557261596d21c82b99076d97d7069539c1
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-03 14:53:07 +0000

    Valores Max Min NaN e Infinito

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,11 +6,11 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            int variavel1 = int.MaxValue;
-            short variavel2 = short.MaxValue;
-            byte variavel3 = byte.MaxValue;
-            double variavel4 = double.MaxValue;
-            float variavel5 = float.MaxValue;
+            int variavel1 = int.MinValue;
+            short variavel2 = short.MinValue;
+            byte variavel3 = byte.MinValue;
+            double variavel4 = double.NaN;
+            float variavel5 = float.PositiveInfinity;
 
             Console.WriteLine(variavel1);
             Console.WriteLine(variavel2);
